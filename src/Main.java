f40c8054254a0358e065b6ce14005c65e2fe8bfb
FICMena <ficm.programming@gmail.com>
2023-06-09 19:07:27 -0300

    Arrays BIDIMENSIONALES

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int numeros [][] = {
+                {10, 20, 30, 40, 50},
+                {10, 20, 30, 40, 50}
+        };
+
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros.length; j++) {
+                System.out.println(numeros[j]);
+            }
+        }
+    }
+}
